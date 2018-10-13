18c42eb5b453caec7a2cdb64118604cc7a4421db
daisuke0115 <nekoha_kotatsude@yahoo.co.jp>
2018-10-13 19:44:57 +0900

    不正チェックを追加

--- copy3.sh
+++ copy3.sh
@@ -1,13 +1,23 @@
 #!/bin/bash 
+#
+# usase:$basename $1
+# $1: パラメータ名
+#
+########################
 set -x
-argument=${1:?引数がありません}
-
+argument=${1}
 conf=/root/etc/copy.conf
 
 # アスタリスクで展開されないように修正
-list=$(grep ^${argument} ${conf} | awk -F "," '{print $2}')
-list2=$(grep ^${argument} ${conf} | awk -F "," '{print $3}')
-list4=$(grep ^${argument} ${conf} | awk -F "," '{print $4}')
+list=$(grep ^${argument} ${conf} | awk -F "," '{print $2}')  # コピー元
+list2=$(grep ^${argument} ${conf} | awk -F "," '{print $3}') # コピー先
+list4=$(grep ^${argument} ${conf} | awk -F "," '{print $4}') # カレンダーフラグ
+
+# 引数の不正チェック
+if [[ $(grep ^${argument} ${conf} | wc -l) -ne 1 ]]; then
+  echo "引数は一意になるように指定してください"
+  exit 1
+fi
 
 # アスタリスクがある場合のコピー先のパラメータ不正チェック
 if [[ `echo "${list}" | grep \*` && ! -d ${list2} ]]; then
@@ -15,8 +25,9 @@ if [[ `echo "${list}" | grep \*` && ! -d ${list2} ]]; then
   exit 1
 fi
 
-for i in $(echo ${list}); do
-  if [[ -r ${i} ]]; then
+# コピー元ファイルが存在し、コピー先ディレクトリがある場合にコピーを実施 
+for i in $(echo ${list:-null}); do
+  if [[ -r ${i} && -d ${list2%/*} ]]; then
     cp ${i} ${list2}
     RC=$?
       if [ $RC -eq 0 ]; then
@@ -26,8 +37,13 @@ for i in $(echo ${list}); do
         exit 1      
       fi
   else
-    echo "File does not exist"
-    exit 1
+# カレンダーフラグがある場合はコピー元が無くても正常終了とする 
+    if [[ ${list4} = cal ]]; then
+      echo "Calender File does not exit. Normal End."
+      exit 0
+    else
+      echo "コピー元、またはコピー先のパラメータが不正です。"
+      exit 1
+    fi
   fi
 done
-

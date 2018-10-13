d3f2af02af7af4e00d40ef889c18690861154768
daisuke0115 <nekoha_kotatsude@yahoo.co.jp>
2018-10-13 18:25:32 +0900

    アスタリスク使用時の不正チェックを追加

--- copy3.sh
+++ copy3.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-# $1 識別子を指定
 set -x
 argument=${1:?引数がありません}
 
@@ -10,34 +9,25 @@ list=$(grep ^${argument} ${conf} | awk -F "," '{print $2}')
 list2=$(grep ^${argument} ${conf} | awk -F "," '{print $3}')
 list4=$(grep ^${argument} ${conf} | awk -F "," '{print $4}')
 
-if [[ ! -d ${list2} && -d ${list2%/*} ]]; then
-else
-  if [[ $(cat ${list} |wc -l) -lt 1 ]]
+# アスタリスクがある場合のコピー先のパラメータ不正チェック
+if [[ `echo "${list}" | grep \*` && ! -d ${list2} ]]; then
+  echo "アスタリスクを使用する場合はコピー先をディレクトリで指定してください。"
+  exit 1
 fi
 
-for i in $(echo ${list:-null}); do #不正な引数対策でechoで展開。
-  if [[ -r ${i} && -d ${list2%/*} ]]; then
-# コピー先の不正チェック 
-    if [[ ! -d ${list2} && -d ${list2%/*} ]]; then
-      cp ${i} ${list2}
-      RC=$?
-        if [ $RC -eq 0 ]; then
-          echo "COPY OK"
-        else
-          echo "COPY NG"
-          exit 1      
-        fi
-    else
-      echo ""
-    fi
+for i in $(echo ${list}); do
+  if [[ -r ${i} ]]; then
+    cp ${i} ${list2}
+    RC=$?
+      if [ $RC -eq 0 ]; then
+        echo "COPY OK"
+      else
+        echo "COPY NG"
+        exit 1      
+      fi
   else
-    if [[ ${list4} = cal ]]; then
-      echo "Calender File does not exit. Normal End."
-      exit 0
-    else
-      echo "File does not exist. Abnormal End."
-      exit 1
-    fi
+    echo "File does not exist"
+    exit 1
   fi
 done
 

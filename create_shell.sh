143dd13af8ab0160fb2b9356a99cdd3e2da359fa
daisuke0115 <nekoha_kotatsude@yahoo.co.jp>
2019-02-10 18:40:52 +0900

    Update create_shell.sh

--- create_shell.sh
+++ create_shell.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+set -ue
 
-shell_name=${1}
+shell_name=$1
 shell_text="#!/bin/bash"
 
-if [ $# = 1 ] ;then
-        echo ${shell_text} >${shell_name} 
-else echo "usage:$0 [shell_name]"
+# check
+if [ $# != 1 ] ;then
+        echo "usage:$0 [shell_name]"
         exit 1
 fi
+if [ -f ${shell_name} ] ;then
+        echo "${shell_name} is alrady exists. Do you want to overwrite?(y/n)"
+        read INPUT
+        if [ $INPUT != "y" ] ;then
+                echo "Can't create Script."
+                exit 1
+        fi
+fi
+
+# main
+echo ${shell_text} >${shell_name} 
 
 if [ -r ${shell_name} ] ;then
         chmod 755 ${shell_name}
-else echo "${shell_name} not find"
+else echo "${shell_name} not found"
         exit 1
 fi
 echo "${shell_name} was created"

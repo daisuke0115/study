1708c35631c081824d1e98bc9e13f40249606012
daisuke0115 <nekoha_kotatsude@yahoo.co.jp>
2019-02-11 23:54:09 +0900

    Update create_shell.sh

--- create_shell.sh
+++ create_shell.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -ue
 
-shell_name=$1
+trap "echo $(basename $0) was ended. RC=$?" 0 1 2 3 4 5 6 8 9 11 15
+shell_name=${1:?usage:$0 [shell_name]}
 shell_text="#!/bin/bash"
 
 # check
-if [ $# != 1 ] ;then
-        echo "usage:$0 [shell_name]"
-        exit 1
-fi
+#if [ $# != 1 ] ;then
+#        echo "usage:$0 [shell_name]"
+#        exit 1
+#fi
 if [ -f ${shell_name} ] ;then
         echo "${shell_name} is alrady exists. Do you want to overwrite?(y/n)"
         read INPUT
@@ -19,7 +20,8 @@ if [ -f ${shell_name} ] ;then
 fi
 
 # main
-echo ${shell_text} >${shell_name} 
+echo "${shell_text}
+set -ue" >${shell_name} 
 
 if [ -r ${shell_name} ] ;then
         chmod 755 ${shell_name}
